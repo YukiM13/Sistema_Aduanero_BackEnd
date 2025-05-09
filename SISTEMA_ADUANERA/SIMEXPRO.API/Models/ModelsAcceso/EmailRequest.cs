3df33783e2ca37bf9533a5e4ef89fe26b57ef4bd
YesusOlivas <jesusantonioolivasflores31@gmail.com>
2025-05-09 07:49:48 -0600

    jesusss

---
+++ SISTEMA_ADUANERA/SIMEXPRO.API/Models/ModelsAcceso/EmailRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace SIMEXPRO.API.Models.ModelsAcceso
+{
+    public class EmailRequest
+    {
+        public string CorreoDestino { get; set; }
+        public string Codigo { get; set; }
+    }
+}
--- SISTEMA_ADUANERA/SIMEXPRO.DataAccess/Repositories/Acce/usuariosRepository.cs
+++ SISTEMA_ADUANERA/SIMEXPRO.DataAccess/Repositories/Acce/usuariosRepository.cs
@@ -5,6 +5,7 @@ using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -35,11 +36,36 @@ namespace SIMEXPRO.DataAccess.Repositories.Acce
 
             parametros.Add("@usua_Nombre", usua_Nombre, DbType.String, ParameterDirection.Input);
 
-            var respuesta = db.QueryFirst<string>(ScriptsDataBase.CorreoSegunUsuario, parametros, commandType: CommandType.StoredProcedure);
+            var respuestadb = db.QueryFirst<string>(ScriptsDataBase.CorreoSegunUsuario, parametros, commandType: CommandType.StoredProcedure);
+
+            var codigo = GenerarCodigo();
+
+            var respuesta = respuestadb + ' ' + codigo;
 
             result.MessageStatus = respuesta;
 
             return result;
+        }
+
+        private const string CaracteresPermitidos = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+
+        public static string GenerarCodigo(int longitud = 6)
+        {
+            var resultado = new StringBuilder();
+            using (var rng = RandomNumberGenerator.Create())
+            {
+                var buffer = new byte[sizeof(uint)];
+
+                for (int i = 0; i < longitud; i++)
+                {
+                    rng.GetBytes(buffer);
+                    uint numero = BitConverter.ToUInt32(buffer, 0);
+                    var index = numero % (uint)CaracteresPermitidos.Length;
+                    resultado.Append(CaracteresPermitidos[(int)index]);
+                }
+            }
+
+            return resultado.ToString();
         }
 
         public RequestStatus CambiarContrasenia(tbUsuarios item)
